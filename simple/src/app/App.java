037093061092d5a821643ee658d145a473cb0e27
Hardik Kapadia <thecoderenroute@gmail.com>
2020-11-09 12:14:18 +0530

    Well there 2

--- simple/src/app/App.java
+++ simple/src/app/App.java
@@ -3,6 +3,6 @@ package app;
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello there!");
-        System.out.println("General Keno-");
+        System.out.println("Genera-");
     }
 }
